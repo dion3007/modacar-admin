d3ecbe0b4b6f1e3af9d98881cc816807b5bc6668
dev4 <yusdion3007@gmail.com>
2021-08-16 03:41:09 +0700

    add

--- src/utils/antares.js
+++ src/utils/antares.js
@@ -1,15 +1,9 @@
 import axios from 'axios';
 
 export const getLatestData = () => {
-  const URL = 'https://platform.antares.id:8443/~/antares-cse/antares-id/Modacar/Raspberry01/la';
-  const headerObj = {
-    'X-M2M-Origin': 'a736ed57b4509598:8ef02d7f4839ddce',
-    'Content-Type': 'application/json;ty=4',
-    'Access-Control-Allow-Origin': '*',
-    Accept: 'application/json'
-  };
+  const URL = 'http://localhost:9000/antares-data';
   axios
-    .get(URL, { headers: headerObj })
+    .get(URL)
     .then((response) => {
       console.log(response.data);
     })
